c642b4a8ba7f89928eac570513deb22c7842e91e
Aaron <proaaron.714@gmail.com>
2017-09-28 09:58:49 -0400

    Test push

---
+++ Lab2/Count_Top15.py
@@ -0,0 +1,16 @@
+fhand = open('TheVictors.txt', 'r')
+counts = dict()
+for line in fhand:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, 0 ) + 1
+
+lst = list()
+for key, val in counts.items():
+    newtup = (val, key)
+    lst.append(newtup)
+
+lst = sorted(lst, reverse = True)
+
+for val, key in lst[:15] :
+    print(key, val)
